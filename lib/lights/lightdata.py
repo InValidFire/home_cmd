5dcf60cd6ee95eae9a8080607090e927c0ce26e4
Riley Housden <riley.t.housden@gmail.com>
2023-05-03 02:49:39 -0400

    change LightData path to property

--- lib/lights/lightdata.py
+++ lib/lights/lightdata.py
@@ -5,3 +5,15 @@ from ..homedata import HomeData
 class LightData(HomeData):
     def __init__(self) -> None:
         self.path = Path.home().joinpath(".storage/lights.json")
+
+    @property
+    def path(self):
+        return self._path
+    
+    @path.setter
+    def path(self, value: Path):
+        if isinstance(value, Path):
+            value.parent.mkdir(parents=True)
+            self._path = value
+        else:
+            raise TypeError(value)
